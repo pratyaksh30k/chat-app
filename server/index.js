d335396121d74c008afab7ca7e65b2ab0188f473
Pratyaksh Kothari <kotharipratyaksh30@gmail.com>
2025-03-08 18:57:56 +0530

    Server Setup

---
+++ server/index.js
@@ -0,0 +1,28 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import mongoose from "mongoose";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors({
+  origin:[process.env.ORIGIN],
+  methods:["GET","POST","PUT","DELETE","PATCH"],
+  credentials:true
+}));
+
+app.use(cookieParser());
+
+app.use(express.json());
+
+const port = process.env.PORT;
+const databaseUrl = process.env.DATABASE_URL;
+
+const server = app.listen(port,()=>{
+  console.log(`Server is running at http://localhost:${port}`);
+});
+
+mongoose.connect(databaseUrl).then(()=>console.log("DB connected successfully.")).catch((err)=>console.log(err.message));
