bee2ce548f42cd5988b156e8edc3a632df72bb83
Pratyaksh Kothari <kotharipratyaksh30@gmail.com>
2025-03-18 12:05:29 +0530

    Empty Chat Screen

--- client/src/pages/chat/components/empty-chat-container/index.jsx
+++ client/src/pages/chat/components/empty-chat-container/index.jsx
@@ -1,9 +1,22 @@
+import { animationDefaultOptions } from "@/lib/utils";
+import Lottie from "react-lottie";
+
 const EmptyChatContainer = () => {
   return (
-    <div>
-      Empty Chat Container
+    <div className="flex-1 md:bg-[#FFFFFF]  hidden md:flex flex-col justify-center items-center duration-1000 transition-all">
+      <Lottie
+        isClickToPauseDisabled
+        height={200}
+        width={200}
+        options={animationDefaultOptions}
+      />
+      <div className="text-opacity-80 text-[#212121] flex flex-col gap-5 items-center mt-4 lg:text-4xl text-3xl transition-all duration-300 text-center">
+        <h3 className="poppins-medium">
+          Hi<span className="text-[#9E9E9E]">!</span> Welcome to <span className="text-[#9E9E9E]">SyncChat</span> Chat App<span className="text-[#9E9E9E]">.</span>
+        </h3>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default EmptyChatContainer
+export default EmptyChatContainer;
